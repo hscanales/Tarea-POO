b1ef77dfe02502dc0811d3b8f9ec243e17752dbf
Hector Canales <00136317@uca.edu.sv>
2018-03-14 07:24:16 -0600

    Update Final
    Update:
    +Fecha de nacimiento
    +Redes Sociales

--- hc.java
+++ hc.java
@@ -1,15 +1,17 @@
 public class hc{
-	String nombre,apellido,carrera,correo,carne;
+	String nombre,apellido,carrera,correo,carne,nacimiento,redessociales;
 	int edad;
 
 
-	hc(String nombre, String apellido, String carrera,String correo, int edad, String carne){
+	hc(String nombre, String apellido, String carrera,String correo, int edad, String carne,String nacimiento,String redessociales){
 		this.nombre=nombre;
 		this.apellido=apellido;
 		this.carrera=carrera;
 		this.edad=edad;
 		this.correo=correo;
 		this.carne=carne;
+		this.nacimiento=nacimiento;
+		this.redessociales=redessociales;
 	}
 	public void print(){
 		System.out.println("Mis datos son:");
@@ -18,5 +20,7 @@ public class hc{
 		System.out.println("Estudio: " + carrera);
 		System.out.println("Mi Carne es: " + carne);
 		System.out.println("Tengo "+edad+" anios");
+		System.out.println("Naci el: "+nacimiento)
+		System.out.println("Encuentrame en redes sociales como: "+redessociales)
 	}
 }
--- tarea1.java
+++ tarea1.java
@@ -1,6 +1,6 @@
 public class tarea1 {
 	public static void main (String[] args){
-		hc yo=new hc("hector","canales","ingenieria informatica","00136317@uca.edu.sv/hectorcanalessv@gmail.com",19,"00136317");
+		hc yo=new hc("hector","canales","ingenieria informatica","00136317@uca.edu.sv/hectorcanalessv@gmail.com",19,"00136317","14 de noviembre,1998","@hectorcanalessv/HsCanales");
 		yo.print();
 	}
 }
